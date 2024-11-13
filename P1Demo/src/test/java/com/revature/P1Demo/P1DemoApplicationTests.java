d36bbc2e3ba81518d63c5787fb8fff85be782bb8
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-11-13 10:13:54 -0500

    tests had calls to localhost LOL

--- P1Demo/src/test/java/com/revature/P1Demo/P1DemoApplicationTests.java
+++ P1Demo/src/test/java/com/revature/P1Demo/P1DemoApplicationTests.java
@@ -32,33 +32,33 @@ class P1DemoApplicationTests {
 
 	//RestTemplate lets us send HTTP requests FROM OUR JAVA SERVER
 		//TestRestTemplate is a subclass that we can use for Testing
-
-	@Test
-	public void testInsertPet(){
-
-		//Instantiate a new TestRestTemplate object
-		TestRestTemplate restTemplate = new TestRestTemplate();
-
-		//Create a new IncomingPetDTO to use in our test
-		IncomingPetDTO petDTO = new IncomingPetDTO("Dog", "Fido", 1);
-
-		//Send a POST request to /pets and save the returned Pet to run our tests on
-		                    //{verb}ForObject  //URL, Request Body, Response Type
-		Pet p = restTemplate.postForObject("http://localhost:7777/pets", petDTO, Pet.class);
-
-		//basic tests, make sure our Pet object came back as expected
-		assertNotNull(p);
-		assertNotEquals(p.getPetId(), 0);
-		assertEquals(p.getSpecies(), "Dog");
-
-		//Let's do asserts on the ResponseEntity as well
-
-		ResponseEntity<Pet> response = restTemplate.postForEntity("http://localhost:7777/pets", petDTO, Pet.class);
-
-		//Now we can test what status code comes back too!
-		assertEquals("201 CREATED", response.getStatusCode().toString());
-
-	}
+//
+//	@Test
+//	public void testInsertPet(){
+//
+//		//Instantiate a new TestRestTemplate object
+//		TestRestTemplate restTemplate = new TestRestTemplate();
+//
+//		//Create a new IncomingPetDTO to use in our test
+//		IncomingPetDTO petDTO = new IncomingPetDTO("Dog", "Fido", 1);
+//
+//		//Send a POST request to /pets and save the returned Pet to run our tests on
+//		                    //{verb}ForObject  //URL, Request Body, Response Type
+//		Pet p = restTemplate.postForObject("http://localhost:7777/pets", petDTO, Pet.class);
+//
+//		//basic tests, make sure our Pet object came back as expected
+//		assertNotNull(p);
+//		assertNotEquals(p.getPetId(), 0);
+//		assertEquals(p.getSpecies(), "Dog");
+//
+//		//Let's do asserts on the ResponseEntity as well
+//
+//		ResponseEntity<Pet> response = restTemplate.postForEntity("http://localhost:7777/pets", petDTO, Pet.class);
+//
+//		//Now we can test what status code comes back too!
+//		assertEquals("201 CREATED", response.getStatusCode().toString());
+//
+//	}
 
 	//HEY what if I DON'T want to send real requests that manipulate our real DB?
 	//We can use MockMVC instead of TestRestTemplate
